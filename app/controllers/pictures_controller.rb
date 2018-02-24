e7e9f77df1fc4a34a909335cdfc646d260c18014
miriam <miriam5000@gmail.com>
2018-02-23 19:29:38 -0500

    Done.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -17,9 +17,14 @@ class PicturesController < ApplicationController
     redirect_to album_picture_url(@picture.album, @picture)
   end
 
+# was def create
+  # @picture = Picture.new(picture_params)
+  # @picture.save
+  # redirect_to picture_url(@picture) -- use url helper here. 
+  
 
   private
     def picture_params
-      params.require(:picture).permit(:url)
+      params.require(:picture).permit(:url, :album_id) # add album_id? 
     end
 end
